b21970cbd064e6218c8e5ce25129dc8094dc06b5
Louis Alvarez <lalvarez4@stu.edu>
2020-01-30 09:28:53 -0500

    Designing implementation

---
+++ StudentPlanner.py
@@ -0,0 +1,24 @@
+# Student Planner
+# How many sections of each course to be offered? Which student goes into each section?
+
+# todo implement more of the design
+# todo get complete list of courses and prereqs
+
+#List of Student ID's
+studentID = [1234, 0, 0]
+#List of Class ID's
+classID = []
+
+#Total credits per semester
+courseCredits = [0,0,0]
+#Default class seats available
+numOfSeats = 30
+
+#The prereqs for a single course
+prereq = [0,0,1]
+
+
+print()
+
+
+
